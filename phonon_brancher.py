d697d095ab34960e823356f2071b7cb8747db6c8
Wiley Morgan <wiley.s.morgan@gmail.com>
2015-05-05 20:09:11 -0600

    First commit. Added files that will be useful in the phonon enumeration problem; generator.py that generates the group operations from the input generators, binomial_calculater.py that finds the binomial coefficient for a set of numbers, and phonon_brancher.py that will contain the bulk of the new algorithm.

---
+++ phonon_brancher.py
@@ -0,0 +1,9 @@
+#This program is meant to find the possible unique configurations of a
+#set of colors with optional displacement vectors attached to each.
+
+#import neede modules
+import numpy as np
+import generator as gn
+import binomial_calculator as bc
+
+
