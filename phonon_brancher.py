c8640e700c399b59aced7536e2d32ae24f2eefbc
Wiley Morgan <wiley.s.morgan@gmail.com>
2015-05-11 21:59:44 -0600

    Added the arrow_group.py program which takes a set of translations of the lattice and a set of rotations paired with the effect they have on the arrows and returns the combined group with its effect on the arrows. Also updated the phonon_brancher so that it applies the group with its effect on the arrows and checks for equivalent configurations only saving those that it finds to be unique.

--- phonon_brancher.py
+++ phonon_brancher.py
@@ -6,6 +6,7 @@ import numpy as np
 import branch_method as bm
 import Inverse_radix_num as irn 
 from copy import deepcopy
+import arrow_group as ar
 #Find concentration takes a 1D array, col, and returns a 1D array, 
 #Concs, containing the number of times each element in col appears 
 #in the array.
@@ -51,22 +52,23 @@ def color_list(col):
 #advance_arrows takes one of the arrows as input and advances it to
 #the next possible arrow.
 def advance_arrows(arrow):
-    if arrow in(range(1,5)):
-        arrow = arrow%4 + 1
+    x = [1,2,3,0]
+    if arrow in(range(0,4)):
+        arrow = x[arrow]
     else:
-        print("Error, arrow not in expected range. Please ensure that the arrows range between 1 and 4.")
+        print("Error, arrow not in expected range. Please ensure that the arrows range between 0 and 3.")
     return(arrow)
+
 #initialize the neede inputs generators is a 2D array that contains
 #the generators for the group to be used.  col is a 1D array thot
-#contians the colors to be used in the enumeration: a singe letter
-#means the atom is not being displaced, a letter and a number means
-#that the atom is being displaced.
-generators=[[0,1,2,3],[1,2,3,0]]
-col=[[1,1],[1],[1],[2,1]]
-
-#first we need to sort the colors according to size so that we
-#can enumerate the colors that aren't being displaced first.
-col=sorted(col, key=len)
+#contians the colors to be used in the enumeration: a number combined
+#with a negative number, i.e. [1,-1], means the atom is not being
+#displaced, a number combined with a positive number between 0 and 3,
+#i.e. [1,2] means that the atom is being displaced.
+generators=[[0,1,2,3,4,5,6,7,8],[1,2,0,4,5,3,7,8,6],[2,0,1,5,3,4,8,6,7],[3,4,5,6,7,8,0,1,2],[4,5,3,7,8,6,1,2,0],[5,3,4,8,6,7,2,0,1],[6,7,8,0,1,2,3,4,5],[7,8,6,1,2,0,4,5,3],[8,6,7,2,0,1,5,3,4],[2,5,8,1,4,7,0,3,6],[8,7,6,5,4,3,2,1,0],[6,3,0,7,4,1,8,5,2],[8,5,2,7,4,1,6,3,0],[0,3,6,1,4,7,2,5,8],[2,1,0,5,4,3,8,7,6],[6,7,8,3,4,5,0,1,2]]
+trans=[[0,1,2,3,4,5,6,7,8],[1,2,0,4,5,3,7,8,6],[2,0,1,5,3,4,8,6,7],[3,4,5,6,7,8,0,1,2],[4,5,3,7,8,6,1,2,0],[5,3,4,8,6,7,2,0,1],[6,7,8,0,1,2,3,4,5],[7,8,6,1,2,0,4,5,3],[8,6,7,2,0,1,5,3,4]]
+rots=[[[0,1,2,3,4,5,6,7,8],[0,1,2,3]],[[2,5,8,1,4,7,0,3,6],[1,2,3,0]],[[8,7,6,5,4,3,2,1,0],[2,3,0,1]],[[6,3,0,7,4,1,8,5,2],[3,0,1,2]],[[8,5,2,7,4,1,6,3,0],[1,0,3,2]],[[0,3,6,1,4,7,2,5,8],[3,2,1,0]],[[2,1,0,5,4,3,8,7,6],[0,3,2,1]],[[6,7,8,3,4,5,0,1,2],[2,1,0,3]]]
+col=[[1,1],[1,-1],[1,-1],[2,-1],[1,-1],[1,-1],[1,-1],[1,-1],[1,-1]]
 
 #We also need to know the concentrations of the atoms as they 
 #occure in the sorted list. This will be stored in concs.
@@ -104,9 +106,9 @@ survivor = []
 for col in configs:
     function = np.zeros(len(col))
     for i in range(len(col)):
-        if len(col[i]) == 1:
-            function[i] = 0
-        elif len(col[i]) == 2:
+        if col[i][1] < 0:
+            function[i] = -1
+        else:
             function[i] = 4
 
     #Set the branch to match the configuration of the colors that we are
@@ -124,16 +126,16 @@ for col in configs:
         visited = 0
         #while there are arrows that still need to be rotated keep doing
         #this. When all the elements of funtcion are zero we are done.
-        while (any(function !=0) or visited != 1):
+        while (any(function > 0) or visited != 1):
             visited = 0
-            if len(col[i]) == 1:
+            if col[i][1] < 0:
                 branch[i]=branch[i]
-            elif len(col[i])==2:
+            elif col[i] >= 0:
                 branch[i][1] = advance_arrows(branch[i][1])
                 function[i] -= 1
             else:
                 print("ERROR: cannot have an entry with more than two components.")
-
+            
             #If we reached the last element in our configuration save it
             #to the survivors.
             if i == len(col)-1:
@@ -153,17 +155,45 @@ for col in configs:
                 if any(function !=0):
                     mv = 0
                     while mv ==0 and i >= 0:
-                        if function[i] ==0 and any(function != 0):
-                            if len(col[i]) == 2:
+                        if function[i] <= 0 and any(function > 0):
+                            if function[i] == 0:
                                 function[i] =4
                             i -= 1
                         else:
                             mv = 1
 
 #done
-print('done')
+print('Found all permutations of the arrows for the unique configurations. They are:')
 
 for i in survivor:
     print(i)
-print(len(survivor))            
 
+print('Now combining the effect of the arrows with the group action.')
+agroup = ar.a_group(trans,rots)
+
+print('Done!')
+print('Now finding unique arrows and configurations using the group combined with the arrow transformations.')
+
+arsurvivors = []
+tempsurv = []
+for x in survivor:
+    for i in agroup:
+        lnew=[]
+        arrow_perm = i[1]
+        for j in i[0]:
+            nar = []
+            nar.append(x[j][0])
+            if x[j][1] >= 0:
+                nar.append(arrow_perm[x[j][1]])
+            else:
+                nar.append(x[j][1])
+            lnew.append(nar)
+        if lnew not in tempsurv and lnew != x:
+            tempsurv.append(deepcopy(lnew))
+    if x not in tempsurv and x not in arsurvivors:
+        arsurvivors.append(x)
+
+print('Finished, the unique arrow and color configurations are:')
+for i in arsurvivors:
+    print(i)
+print(len(arsurvivors))
